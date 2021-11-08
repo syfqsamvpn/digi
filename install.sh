8e6cd38d1ae1516bea9c27604771b94deabcce28
syfqsamvpn <89333348+syfqsamvpn@users.noreply.github.com>
2021-11-08 23:06:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 # download script
 cd /usr/bin
-wget -O menu "https://raw.githubusercontent.com/syfqsamvpn/digi/main/menu.sh"
+wget -O digi "https://raw.githubusercontent.com/syfqsamvpn/digi/main/digi.sh"
 wget -O 3gb "https://raw.githubusercontent.com/syfqsamvpn/digi/main/3gb.sh"
 wget -O 6gb "https://raw.githubusercontent.com/syfqsamvpn/digi/main/6gb.sh"
 wget -O cookie "https://raw.githubusercontent.com/syfqsamvpn/digi/main/cookie.sh"
 wget -O unlimited "https://raw.githubusercontent.com/syfqsamvpn/digi/main/unlimited.sh"
 wget -O extend "https://raw.githubusercontent.com/syfqsamvpn/digi/main/extend.sh"
-chmod +x menu
+chmod +x digi
 chmod +x 3gb
 chmod +x 6gb
 chmod +x cookie
